b1d747c0407d633ec8e551844d44a3de76054657
Sleep Kuntz <sleep@kuntz.co>
2023-11-27 22:50:05 -0600

    remove commented out line

--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,6 @@ export default defineConfig({
       output: {
         manualChunks(id) {
           if (id.includes("node_modules")) {
-            // return id.toString().split('node_modules/')[1].split('/')[0].toString();
             return "vendor"
           }
         }
